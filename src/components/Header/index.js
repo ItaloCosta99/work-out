e92adfd6bf6003d098e850143baf01be2dbc006a
Italo Costa <italocostajobs@gmail.com>
2021-10-17 16:12:57 -0300

    fix: import de arquivo e especificação de classe

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,12 @@
 import './style.css';
-import closeerIcon from '../../image/svg/closeer-light.svg';
+import closeerIconLight from '../../image/svg/closeer-light.svg';
 import { Profile } from './Profile/index';
 
 export function Header() {
   return (
     <header className="header-container">
       <div className="wrapper">
-        <img className="closeer-icon" src={closeerIcon} alt="Closeer Ícone" />
+        <img className="closeer-icon" src={closeerIconLight} alt="Closeer Ícone" />
         <h1 className="current-time">Hoje - 15 Janeiro de 2021 - 12:00</h1>
         <Profile />
       </div>
