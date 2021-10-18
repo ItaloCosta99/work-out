48b5d9cff5a3d9145f2dcca04bb7d4d55903617f
Italo Costa <italocostajobs@gmail.com>
2021-10-18 00:59:48 -0300

    fix: input height

--- src/components/Main/Form/index.js
+++ src/components/Main/Form/index.js
@@ -2,16 +2,18 @@ import './style.css';
 import { FiCalendar } from 'react-icons/fi'
 
 export function Form() {
-  return(
+  return (
     <form className="form-container" method="POST" action="">
       <input type="date" placeholder="Selecione uma data..." />
 
       <div>
         <input type="text" placeholder="Hora" />
-        <span></span>
+        <span className="white-bar"></span>
         <input type="text" placeholder="Minuto" />
       </div>
-      <button className="form-button"><FiCalendar size="14" color="white"/> <span id="bar"></span>Agendar</button>
+      <div>
+        <button className="form-button"><FiCalendar size="14" color="white" /><span className="white-bar"></span>Agendar</button>
+      </div>
     </form>
   );
 }
